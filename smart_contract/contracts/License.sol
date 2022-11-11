8ee42275c4f08276b8ea2d4c32d83b912391e6e7
NetradeepakC <netradeepakc@gmail.com>
2022-11-11 15:02:54 +0530

    Starting implementing security measures

--- smart_contract/contracts/License.sol
+++ smart_contract/contracts/License.sol
@@ -4,24 +4,38 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Misc.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, Ownable {
-    constructor() ERC721("MyToken", "MTK") {}
+contract MediStore is ERC721, ERC721URIStorage, Ownable, Misc {
+    constructor() ERC721("MediStore", "MediStore") {}
 
-    event Attest(address indexed to, uint256 indexed tokenId);
+    ////BASIC NFT FUNCTIONS
 
-    event Revoke(address indexed to, uint256 indexed tokenId);
+    mapping(address => uint256[]) internal tokenList;
 
     function safeMint(
         address to,
-        uint256 tokenId,
-        string memory uri
+        uint16[] memory tokenIdParts,
+        string memory uri,
+        uint16[] memory seedParts,
+        string memory timeParts
     ) public onlyOwner {
+        ////JS INTEERFACE
+
+        uint256 tokenId = combine(tokenIdParts);
+        uint256 seed = combine(seedParts);
+
+        ////BASIC NFT FUNCTION
+
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-    }
 
-    // The following functions are overrides required by Solidity.
+        ////SECURITY INIT
+
+        updateSeed(tokenId, seed);
+        timeLent[tokenId] = block.timestamp;
+        tokenList[msg.sender].push(tokenId);
+    }
 
     function _burn(uint256 tokenId)
         internal
@@ -39,10 +53,42 @@ contract MyToken is ERC721, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
+    ////DIFFERENTIAL SECURITY SYSTEM
+
+    mapping(uint256 => uint256) seedMap;
+    mapping(uint256 => string) timeLent;
+
+    function updateSeed(uint256 tokenId, uint16[] seedParts) internal {
+        seed = combine(seedParts);
+        seedMap[tokenId] = seed;
+    }
+
+    function getCurrentTime() public view returns (string memory) {
+        return uintToString(block.timestamp);
+    }
+
+    function getLastUpdatedTime(uint256 tokenId)
+        public
+        view
+        onlyOwner
+        returns (string)
+    {
+        return uintToString(timeLent[tokenId]);
+    }
+
+    function getTokenList() public view returns (uint256[] memory) {
+        return tokenList[msg.sender];
+    }
+
+    ////SOUL BOUND SYSTEM
+
+    event Attest(address indexed to, uint256 indexed tokenId);
+    event Revoke(address indexed to, uint256 indexed tokenId);
+
     function _beforeTokenTransfer(
         address from,
         address to,
-        uint256 firstTokenId,
+        uint256,
         uint256 batchSize
     ) internal virtual override {
         require(batchSize == 0, "Batch minting not allowed");
---
+++ smart_contract/contracts/Misc.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Misc {
+    function combine(uint16[] memory arr) internal pure returns (uint256) {
+        uint256 out = 0;
+        for (uint256 i = 16; i >= 1; i--) {
+            out = (out << 16) | uint256(arr[i - 1]);
+        }
+        return out;
+    }
+
+    function uintToString(uint256 v) internal pure returns (string memory) {
+        bytes memory reversed = new bytes(100);
+        uint256 i;
+        for (i = 0; v > 0; i++) {
+            reversed[i] = bytes1(uint8(48 + (v % 10)));
+            v /= 10;
+        }
+        bytes memory str = new bytes(reversed.length);
+        for (uint256 j = 0; j < reversed.length; j++) {
+            str[j] = reversed[reversed.length - j - 1];
+        }
+        return string(str);
+    }
+}
