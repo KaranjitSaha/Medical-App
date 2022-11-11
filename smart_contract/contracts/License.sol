318546c149c8173d791a70dd5f6440a25b97c23c
NetradeepakC <netradeepakc@gmail.com>
2022-11-11 12:15:22 +0530

    Made contract soulbound

--- smart_contract/contracts/License.sol
+++ smart_contract/contracts/License.sol
@@ -8,6 +8,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyToken is ERC721, ERC721URIStorage, Ownable {
     constructor() ERC721("MyToken", "MTK") {}
 
+    event Attest(address indexed to, uint256 indexed tokenId);
+
+    event Revoke(address indexed to, uint256 indexed tokenId);
+
     function safeMint(
         address to,
         uint256 tokenId,
@@ -33,5 +37,32 @@ contract MyToken is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override {
+        require(batchSize == 0, "Batch minting not allowed");
+        require(
+            from == address(0) || to == address(0),
+            "Soulbound tokens can't be tranfered."
+        );
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override {
+        require(batchSize == 0, "Batch burning not allowed");
+        if (from == address(0)) {
+            emit Attest(to, firstTokenId);
+        } else if (to == address(0)) {
+            emit Revoke(to, firstTokenId);
+        }
     }
 }
