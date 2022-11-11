3b0c73edd644490bbfb483b72bb29ceeeb098884
Rudransh Dixit <dixitrudransh01@gmail.com>
2022-11-11 20:34:58 +0530

    Added basic tests

--- smart_contract/test/Lock.js
+++ smart_contract/test/Lock.js
@@ -14,11 +14,25 @@ describe('MedRecord.sol', () => {
 
     const MedRecord = await ethers.getContractFactory('MedRecord')
     medRecord = await MedRecord.deploy()
+
+    // Creating a basic NFT
+    let transaction = await medRecord.connect(customer).safeMint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS", 10, { 'time': "10:00", 'group': "Prescription", "issue": "10:00" })
+    await transaction.wait()
+
+    transaction = await medRecord.connect(customer).safeMint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS", 10, { 'time': "10:00", 'group': "Prescription", "issue": "10:00" })
+    await transaction.wait()
   })
-  describe('Deploy', async () => {
-    it('Creating a Basic NFT', async () => {
-      let transaction = await medRecord.connect(customer).safeMint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS", 10, { 'time': "10:00", 'group': "Prescription", "issue": "10:00" })
-      await transaction.wait()
+
+  describe('basic tests', () => {
+    it('checking for get token', async () => {
+      let result = await medRecord.tokenURI(1)
+      expect(result).to.be.equal("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+    })
+
+    it('getting the token list', async () => {
+      let result = await medRecord.getTokenList()
+      console.log(result)
+      expect(1).to.be.equal(1)
     })
   })
 
