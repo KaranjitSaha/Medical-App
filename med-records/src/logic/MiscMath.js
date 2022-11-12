1f22cb3f3b0fd9ab07d132a7fd39ee0a4ac88fa6
Rudransh <dixitrudransh01@gmail.com>
2022-11-13 03:50:42 +0530

    added signin

--- med-records/src/logic/MiscMath.js
+++ med-records/src/logic/MiscMath.js
@@ -1,59 +1,83 @@
-// const { BigNumber } = require('ethers')
-// const CryptoJS = require("crypto-js")
-// const Cryptr = require('cryptr');
-// const { createHash } = require('crypto-browserify');
+const { BigNumber } = require('ethers')
+const aesjs = require('aes-js');
+const sha256 = require('js-sha256');
 
-// getRandom16 = function () {
-//     let rand = 1 << 16;
-//     rand = rand * Math.random();
-//     return Math.floor(rand);
-// }
-// exports.getRandom16 = getRandom16
+let getRandom16 = function () {
+    let rand = 1 << 16;
+    rand = rand * Math.random();
+    return Math.floor(rand);
+}
+exports.getRandom16 = getRandom16
 
-// exports.split16 = function (bgn) {
-//     let base = 1 << 16;
-//     split = [];
-//     while (!bgn.isEqualTo(0)) {
-//         split.push((bgn.modulo(base)).toNumber());
-//         bgn = bgn.dividedToIntegerBy(base);
-//     }
-//     return split;
-// }
+let split16 = function (bgn) {
+    let base = 1 << 16;
+    let split = [];
+    while (!bgn.isEqualTo(0)) {
+        sha256 = require('js-sha256');
 
-// combine16 = function (rnd) {
-//     var out = BigNumber.from(0)
-//     for (var i = 0; i < 16; i++) {
-//         out = out.shl(16);
-//         out = out.or(rnd[i]);
-//     }
-//     // rnd.array.forEach(i => {
-//     //     out = out.shl(16);
-//     //     out = out.or(i);
-//     // });
-//     return out;
-// }
-// exports.combine16 = combine16
+        split.push((bgn.modulo(base)).toNumber());
+        bgn = bgn.dividedToIntegerBy(base);
+    }
+    return split;
+}
+exports.split16 = split16
 
-// exports.getRandom256 = function () {
-//     var arr = []
-//     for (var i = 0; i < 16; i++) {
-//         arr.push(getRandom16());
-//     }
-//     return combine16(arr);
-// }
+let split8 = function (bgn) {
+    let base = 1 << 8;
+    let split = [];
+    while (!bgn.isEqualTo(0)) {
+        split.push((bgn.modulo(base)).toNumber());
+        bgn = bgn.dividedToIntegerBy(base);
+    }
+    return split;
+}
+exports.split8 = split8
 
-// exports.Encrypt = function (object, hash) {
-//     // return CryptoJS.AES.encrypt(JSON.stringify(object), hash.toString()).toString();
-//     const cryptr = new Cryptr(hash.toString());
-//     return cryptr.encrypt(JSON.stringify(object))
-// }
+let combine16 = function (rnd) {
+    var out = BigNumber.from(0)
+    for (var i = 0; i < 16; i++) {
+        out = out.shl(16);
+        out = out.or(rnd[i]);
+    }
+    // rnd.array.forEach(i => {
+    //     out = out.shl(16);
+    //     out = out.or(i);
+    // });
+    return out;
+}
+exports.combine16 = combine16
 
-// exports.Decrypt = function (cipherText, hash) {
-//     // return CryptoJS.AES.decrypt(cipherText, hash.toString()).toString(CryptoJS.enc.Utf8);
-//     const cryptr = new Cryptr(hash.toString());
-//     return cryptr.decrypt(JSON.stringify(object))
-// }
+exports.getRandom256 = function () {
+    var arr = []
+    for (var i = 0; i < 16; i++) {
+        arr.push(getRandom16());
+    }
+    return combine16(arr);
+}
 
-// exports.Hash = function (plainText) {
-//     return createHash('sha256').update(plainText).digest('hex');
-// }
+let Encrypt = function (object, hash) {
+    // return CryptoJS.AES.encrypt(JSON.stringify(object), hash.toString()).toString();
+    var textBytes = aesjs.utils.utf8.toBytes(object.toString());
+    var aesCtr = new aesjs.ModeOfOperation.ctr(hash);
+    var encryptedBytes = aesCtr.encrypt(textBytes);
+    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+    return encryptedHex;
+}
+exports.Encrypt = Encrypt
+
+let Decrypt = function (cipherText, hash) {
+    // return CryptoJS.AES.decrypt(cipherText, hash.toString()).toString(CryptoJS.enc.Utf8);
+    var encryptedBytes = aesjs.utils.hex.toBytes(cipherText);
+    var aesCtr = new aesjs.ModeOfOperation.ctr(hash);
+    var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+    return decryptedText;
+}
+exports.Decrypt = Decrypt
+
+let Hash = function (plainText) {
+    // return createHash('sha256').update(plainText).digest('hex');
+    console.log(plainText)
+    return sha256.array(plainText.toString());
+}
+exports.Hash = Hash
