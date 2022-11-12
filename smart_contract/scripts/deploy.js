889df628ea9a8929ad1fdfc22bd1e0bf2150ae3b
Rudransh Dixit <dixitrudransh01@gmail.com>
2022-11-12 16:05:26 +0530

    cleaup

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -6,28 +6,11 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-export let medRecord;
+let medRecord;
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
   const MedRecord = await hre.ethers.getContractFactory("MedRecord")
   const medRecord = await MedRecord.deploy()
-
-  console.log(medRecord.address)
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
